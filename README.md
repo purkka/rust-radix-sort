fcfee82e10f72af0277fce919d12116a7fb88e77
Erika Marttinen <erika.marttinen@outlook.com>
2021-07-16 21:09:13 +0300

    Add performance comparison graph

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ A simple radix sort algorithm for 32-bit signed integers.
 - `cargo test` for tests
 - `cargo bench` for benchmarks
 
+---
+
+### Performance Comparison
+
+The performance of this radix sort implementation compared to Rust's std::vec::Vec::sort.
+The graph has logarithmic axes for better visualization. "Input" refers to the size of
+the given vector.
+
+![alt text](./resources/comparison-ver1.png "Log axis graph comparing the sorting algorithms")
+
+---
 
 \
 Erika Marttinen 2021
--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,9 +5,8 @@ use rust_radix_sort::{radix_sort, generate_vector};
 // std::sort() function with large vectors. Uses Criterion for comparisons and plotting graphs.
 fn bench_sorts(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sort vs Radix Sort");
-    // Plot the results in on a logarithmic graph.
-    // let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
-    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Linear);
+    // Plot the results on a logarithmic graph.
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     // Currently benches vectors containing up to one million integers.
     let range = 0..=6;
     let vecs = range.clone().map(|z| (z, generate_vector(z as u32))).collect::<Vec<_>>();
@@ -15,9 +14,10 @@ fn bench_sorts(c: &mut Criterion) {
     // Benchmark radix sort for each vector one by one.
     for (index, vec) in vecs {
         let current = vec;
-        group.bench_with_input(BenchmarkId::new("Radix Sort", index), &current,
+        let id = i32::pow(10, index);
+        group.bench_with_input(BenchmarkId::new("Radix Sort", id), &current,
                                 |b, v| b.iter(|| radix_sort(v))).plot_config(plot_config.clone());
-        group.bench_with_input(BenchmarkId::new("Sort", index), &current,
+        group.bench_with_input(BenchmarkId::new("Sort", id), &current,
                                 |b, v| b.iter(|| v.clone().sort())).plot_config(plot_config.clone());
     }
     group.finish();
